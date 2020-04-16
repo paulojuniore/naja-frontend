5ce150506b94d984ca9afa56fe1f641f38c8a0dc
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-04-16 20:34:53 -0300

    Tela de login básica finalizada

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import './styles.css';
+
+import { Link } from 'react-router-dom';
+
 export default function Login() {
   return (
-    <h1>Login page</h1>
+    <div className='login-container'>
+      <section className='form'>
+        <form action="">
+          <h1>Naja Store</h1>
+          <h1>Faça seu login, administrador!</h1>
+          <input 
+            placeholder='Seu e-mail' />
+
+          <input
+            placeholder='Sua senha'
+            type='password' />
+          
+          <button className='button' type='submit'>Entrar</button>
+
+          <Link style={{marginTop: 10}} className='link-register' to='/register'>
+            Cadastre-se
+          </Link>
+        </form>
+      </section>
+    </div>
   )
 }
