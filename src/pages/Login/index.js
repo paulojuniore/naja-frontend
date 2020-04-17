bb1463310889cf39974f07fd9afd222b5d00eeff
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-04-17 13:35:27 -0300

    Capturando dados dos inputs da tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
     <div className='login-container'>
       <section className='form'>
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h1>Naja Store</h1>
           <h1>Faça seu login, administrador!</h1>
           <input 
-            placeholder='Seu e-mail' />
+            placeholder='Seu e-mail'
+            value={email}
+            onChange={e => setEmail(e.target.value)} />
 
           <input
             placeholder='Sua senha'
-            type='password' />
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)} />
           
           <button className='button' type='submit'>Entrar</button>
 
