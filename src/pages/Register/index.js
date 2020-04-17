955699008aae4db93101ab8322f1989569f60b6e
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-04-17 13:42:58 -0300

    Capturando dados dos inputs da tela de cadastro.

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 import { Link } from 'react-router-dom';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  function handleRegister(e) {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm
+    })
+  }
+
   return (
     <div className="register-container">
       <section className="form">
-        <form>
+        <form onSubmit={handleRegister}>
           <h1>Cadastro - Naja Store</h1>
 
           <label className='register-title'>Nome *</label>
           <input
-            placeholder='Digite seu nome' />
+            placeholder='Digite seu nome'
+            value={name}
+            onChange={e => setName(e.target.value)} />
 
           <label className='register-title'>Email *</label>
           <input
-            placeholder='Digite seu email' />
+            placeholder='Digite seu email'
+            value={email}
+            onChange={e => setEmail(e.target.value)} />
 
           <label className='register-title'>Senha *</label>
           <input 
-            placeholder='Digite sua senha' />
+            placeholder='Digite sua senha'
+            value={password}
+            onChange={e => setPassword(e.target.value)} />
 
           <input
-            placeholder='Confirme sua senha' />
+            placeholder='Confirme sua senha'
+            value={passwordConfirm}
+            onChange={e => setPasswordConfirm(e.target.value)} />
           
           <button className='button' type='submit'>Cadastrar</button>
 
