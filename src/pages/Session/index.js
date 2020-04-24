0d046c75a659830723ce51389bc3480e512c9e32
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-04-24 13:11:44 -0300

    Iniciando listagem dos itens (apenas css)

--- src/pages/Session/index.js
+++ src/pages/Session/index.js
@@ -2,10 +2,36 @@ import React from 'react';
 
 import Header from '../Header';
 
+import './styles.css';
+
 export default function Session() {
   return (
     <>
-      <Header />
+    <Header />
+    <div className="container">
+      <ul className="container-products">
+        <li>
+          <div className="box-product">
+            <strong>Nome do produto</strong>
+          </div>
+        </li>
+        <li>
+          <div className="box-product">
+            <strong>Nome do produto</strong>
+          </div>
+        </li>
+        <li>
+          <div className="box-product">
+            <strong>Nome do produto</strong>
+          </div>
+        </li>
+        <li>
+          <div className="box-product">
+            <strong>Nome do produto</strong>
+          </div>
+        </li>
+      </ul>
+    </div>
     </>
   )
 }
